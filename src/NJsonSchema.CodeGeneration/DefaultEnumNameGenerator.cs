e0217b49249f6a13ae4a5f1ec6d5b5627d6c35c8
Chris Williams <chris.williams@spreetail.com>
2025-01-23 20:28:02 -0600

    Update DefaultEnumNameGenerator.cs to handle plus characters

--- src/NJsonSchema.CodeGeneration/DefaultEnumNameGenerator.cs
+++ src/NJsonSchema.CodeGeneration/DefaultEnumNameGenerator.cs
@@ -35,7 +35,8 @@ namespace NJsonSchema.CodeGeneration
                 .Replace(",", "_")
                 .Replace("#", "_")
                 .Replace("-", "_")
-                .Replace("\\", "_");
+                .Replace("\\", "_")
+                .Replace("+", "_");
         }
     }
 }
